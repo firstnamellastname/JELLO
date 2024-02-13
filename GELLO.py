da443021160f4ffb3b4da0c14857fad3a795fc29
firstnamellastname <Marulendran10@srrcc.org.uk>
2024-02-13 16:40:26 +0000

    added a rectangle to act as an arena ?

--- GELLO.py
+++ GELLO.py
@@ -1,55 +1,54 @@
-import pygame
+import pygame as pg
 
 # pygame setup
-pygame.init()
+pg.init()
 
-pygame.display.set_caption("JELLO")
+pg.display.set_caption("JELLO")
 width = 1200
 height = 720
 image_height = 40
 image_width = 40
+screen_colour = (0, 0, 0)
+screen = pg.display.set_mode((width, height))
 
-screen = pygame.display.set_mode((width, height))
+x = width/2
+y = height/2
 
-x = 600
-y = 360
-
-clock = pygame.time.Clock()
+clock = pg.time.Clock()
 
 running = True
 
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 10, screen.get_height() / 10)
+player_pos = pg.Vector2(screen.get_width() / 10, screen.get_height() / 10)
 
 while running:
     # poll for events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
 
 
-    screen.fill("black")
+    screen.fill(screen_colour)
 
-    heart_sprite = pygame.image.load("Undertaleheart.png")
+    heart_sprite = pg.image.load("Undertaleheart.png")
     IMAGE_SIZE = (image_width,image_height)
-    heart_sprite = pygame.transform.scale(heart_sprite, IMAGE_SIZE)
+    heart_sprite = pg.transform.scale(heart_sprite, IMAGE_SIZE)
     screen.blit(heart_sprite, (x, y))
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
+    keys = pg.key.get_pressed()
+    if keys[pg.K_w]:
         y -= 300 * dt
-    if keys[pygame.K_s]:
+    if keys[pg.K_s]:
         y += 300 * dt
-    if keys[pygame.K_a]:
+    if keys[pg.K_a]:
         x -= 300 * dt
-    if keys[pygame.K_d]:
+    if keys[pg.K_d]:
         x += 300 * dt
 
-    #screen.fill((0,0,0))
-  #  pygame.draw.rect(screen, (255,0,0), (x,y, image_width , image_height))
-    pygame.display.flip()
+    pg.draw.rect(screen ,"white" , pg.Rect(435,200,350,350 ), 4)
+    pg.display.flip()
     dt = clock.tick(60) / 1000
 
-pygame.quit()
+pg.quit()
 
